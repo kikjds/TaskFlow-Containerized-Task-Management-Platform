5afebd6e09e9f1db82b30aace02a1e74b80f2d52
macolepe <kick.jds@gmail.com>
2026-07-27 00:02:28 +0200

    Added Redis password parameter to connection setup

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 const client = new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD || undefined,
     maxRetriesPerRequest: null
 });
 
