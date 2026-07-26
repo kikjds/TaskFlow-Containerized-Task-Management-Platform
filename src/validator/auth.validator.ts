655efcf3acf3abd1076482709ff3ffd677adfa23
macolepe <kick.jds@gmail.com>
2026-07-26 23:55:27 +0200

    Added login form validator

--- src/validator/auth.validator.ts
+++ src/validator/auth.validator.ts
@@ -6,3 +6,8 @@ export const authSchema = z.object({
     email: z.string().email({ message: "Invalid email address" }).max(100, { message: "Email must be less than 100 characters" }),
     notifications: z.boolean().optional()
 })
+
+export const loginSchema = z.object({
+    username: z.string().min(1, { message: "Username is required" }).max(50, { message: "Username must be less than 50 characters" }),
+    password: z.string().min(6, { message: "Password must be at least 6 characters" }).max(100, { message: "Password must be less than 100 characters" }),
+})
